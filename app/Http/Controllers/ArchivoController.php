881547d248cbe49d59804b6e54ecb16ec91eb3ae
ToKi-Angel <toki.nunez.evangelista@gmail.com>
2023-06-06 21:58:31 -0600

    Ahora puedo ver los pdf

--- app/Http/Controllers/ArchivoController.php
+++ app/Http/Controllers/ArchivoController.php
@@ -25,26 +25,57 @@ class ArchivoController extends Controller
     }
 }*/
 
+
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ArchivoController extends Controller
 {
     public function mostrarArchivos($matricula)
-{
-    $titulo = 'Mostrar archivos';
-    $rutaCivicas = 'Alumnos/' . $matricula . '/HorasCivicas/MOOC';
-    $rutaDeportivas = 'Alumnos/' . $matricula . '/HorasDeportivas/MOOC';
-    $rutaCulturales = 'Alumnos/' . $matricula . '/HorasCulturales/MOOC';
+    {
+        $titulo = 'Mostrar archivos';
+        $rutaCivicasMOOC = 'Alumnos/' . $matricula . '/HorasCivicas/MOOC';
+        $rutaCivicasEvidencias = 'Alumnos/' . $matricula . '/HorasCivicas/Evidencias';
+        $rutaDeportivasMOOC = 'Alumnos/' . $matricula . '/HorasDeportivas/MOOC';
+        $rutaDeportivasEvidencias = 'Alumnos/' . $matricula . '/HorasDeportivas/Evidencias';
+        $rutaCulturalesMOOC = 'Alumnos/' . $matricula . '/HorasCulturales/MOOC';
+        $rutaCulturalesEvidencias = 'Alumnos/' . $matricula . '/HorasCulturales/Evidencias';
 
-    /*dd($rutaCivicas, $rutaDeportivas, $rutaCulturales);*/
+        $archivoMoocCivico = $rutaCivicasMOOC . '/mooc.pdf';
+        $archivoEvidenciasCivicas = $rutaCivicasEvidencias . '/evidencias.pdf';
+        $archivoMoocDeportivas = $rutaDeportivasMOOC . '/mooc.pdf';
+        $archivoEvidenciasDeportivas = $rutaDeportivasEvidencias . '/evidencias.pdf';
+        $archivoMoocCulturales = $rutaCulturalesMOOC . '/mooc.pdf';
+        $archivoEvidenciasCulturales = $rutaCulturalesEvidencias . '/evidencias.pdf';
 
-    $archivosCivicas = Storage::files($rutaCivicas);
-    $archivosDeportivas = Storage::files($rutaDeportivas);
-    $archivosCulturales = Storage::files($rutaCulturales);
+        $existeMoocCivico = file_exists(public_path($archivoMoocCivico));
+        $existeEvidenciasCivicas = file_exists(public_path($archivoEvidenciasCivicas));
+        $existeMoocDeportivas = file_exists(public_path($archivoMoocDeportivas));
+        $existeEvidenciasDeportivas = file_exists(public_path($archivoEvidenciasDeportivas));
+        $existeMoocCulturales = file_exists(public_path($archivoMoocCulturales));
+        $existeEvidenciasCulturales = file_exists(public_path($archivoEvidenciasCulturales));
 
-    return view('crud.archivos', compact('titulo', 'archivosCivicas', 'archivosDeportivas', 'archivosCulturales'));
+        return view('crud.archivos', compact(
+            'titulo',
+            'archivoMoocCivico',
+            'archivoEvidenciasCivicas',
+            'archivoMoocDeportivas',
+            'archivoEvidenciasDeportivas',
+            'archivoMoocCulturales',
+            'archivoEvidenciasCulturales',
+            'existeMoocCivico',
+            'existeEvidenciasCivicas',
+            'existeMoocDeportivas',
+            'existeEvidenciasDeportivas',
+            'existeMoocCulturales',
+            'existeEvidenciasCulturales',
+            'matricula',
+            'rutaCivicasMOOC',
+            'rutaCivicasEvidencias',
+            'rutaDeportivasMOOC',
+            'rutaDeportivasEvidencias',
+            'rutaCulturalesMOOC',
+            'rutaCulturalesEvidencias'
+        ));
+    }
 }
-
-}
-
--- resources/views/crud/archivos.blade.php
+++ resources/views/crud/archivos.blade.php
@@ -5,36 +5,45 @@
     <div class="card">
         <div class="card-body">
             <h4>Archivos de Horas Cívicas (MOOC):</h4>
-            @if(count($archivosCivicas) > 0)
-                <ul>
-                    @foreach ($archivosCivicas as $archivo)
-                        <li>{{ $archivo }}</li>
-                    @endforeach
-                </ul>
+            @if ($existeMoocCivico)
+                <iframe src="{{ asset($archivoMoocCivico) }}" width="100%" height="300px"></iframe>
             @else
-                <p>No se encontraron archivos de Horas Cívicas.</p>
+                <p>No se encontró el archivo MOOC en las Horas Cívicas.</p>
+            @endif
+
+            <h4>Archivos de Horas Cívicas (Evidencias):</h4>
+            @if ($existeEvidenciasCivicas)
+                <iframe src="{{ asset($archivoEvidenciasCivicas) }}" width="100%" height="300px"></iframe>
+            @else
+                <p>No se encontró el archivo de evidencias en las Horas Cívicas.</p>
             @endif
 
             <h4>Archivos de Horas Deportivas (MOOC):</h4>
-            @if(count($archivosDeportivas) > 0)
-                <ul>
-                    @foreach ($archivosDeportivas as $archivo)
-                        <li>{{ $archivo }}</li>
-                    @endforeach
-                </ul>
+            @if ($existeMoocDeportivas)
+                <iframe src="{{ asset($archivoMoocDeportivas) }}" width="100%" height="300px"></iframe>
             @else
-                <p>No se encontraron archivos de Horas Deportivas.</p>
+                <p>No se encontró el archivo MOOC en las Horas Deportivas.</p>
+            @endif
+
+            <h4>Archivos de Horas Deportivas (Evidencias):</h4>
+            @if ($existeEvidenciasDeportivas)
+                <iframe src="{{ asset($archivoEvidenciasDeportivas) }}" width="100%" height="300px"></iframe>
+            @else
+                <p>No se encontró el archivo de evidencias en las Horas Deportivas.</p>
             @endif
 
             <h4>Archivos de Horas Culturales (MOOC):</h4>
-            @if(count($archivosCulturales) > 0)
-                <ul>
-                    @foreach ($archivosCulturales as $archivo)
-                        <li>{{ $archivo }}</li>
-                    @endforeach
-                </ul>
+            @if ($existeMoocCulturales)
+                <iframe src="{{ asset($archivoMoocCulturales) }}" width="100%" height="300px"></iframe>
             @else
-                <p>No se encontraron archivos de Horas Culturales.</p>
+                <p>No se encontró el archivo MOOC en las Horas Culturales.</p>
+            @endif
+
+            <h4>Archivos de Horas Culturales (Evidencias):</h4>
+            @if ($existeEvidenciasCulturales)
+                <iframe src="{{ asset($archivoEvidenciasCulturales) }}" width="100%" height="300px"></iframe>
+            @else
+                <p>No se encontró el archivo de evidencias en las Horas Culturales.</p>
             @endif
         </div>
     </div>
